7f877fde8cfc0034f94757c46208d79c2def9a51
Valentino Franco <valeenbros@gmail.com>
2026-09-09 10:36:59 -0300

    solved

--- 13. Roman to Integer.java
+++ 13. Roman to Integer.java
@@ -50,6 +50,32 @@ Constraints:
 
 
 
-public class 13. Roman to Integer {
-    
+class Solution {
+    public int romanToInt(String s) {
+        int resultado = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int actual = valor(s.charAt(i));
+
+            if (i + 1 < s.length() && actual < valor(s.charAt(i + 1))) {
+                resultado -= actual;
+            } else {
+                resultado += actual;
+            }
+        }
+
+        return resultado;
+    }
+    private int valor(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
 }
